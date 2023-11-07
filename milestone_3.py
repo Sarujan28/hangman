a8559b74491d31f7336505e0c8ab355d4d0c6dda
Sarujan28 <sarujan28@hotmail.co.uk>
2023-11-07 20:16:04 +0000

    Adding function for iterating loop to check if guess input is valid with inner check guess function

--- milestone_3.py
+++ milestone_3.py
@@ -21,3 +21,27 @@ def check_guess(guess):
         print(f'Good guess! {guess} is in the word.')
     else:
         print(f'Sorry, {guess} is not in the word. Try again.')
+# Ask input function
+def ask_input():
+    
+    import random
+    word_list = ['orange', 'apple', 'mango', 'melon', 'pear']
+    word = random.choice(word_list)
+    print(word)
+    
+    while True:
+        guess = input('Guess a single alphabetical letter: ')
+        if len(guess) == 1 and guess.isalpha() == True:
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+            continue
+    
+    def check_guess(guess):
+        lower_guess = guess.lower()
+        if lower_guess in word:
+            print(f'Good guess! {lower_guess} is in the word.')
+        else:
+            print(f'Sorry, {lower_guess} is not in the word. Try again.')
+     
+    check_guess(guess)
