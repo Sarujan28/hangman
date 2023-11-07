607e949eccb5ce47c609c5b07aa42085345fac74
Sarujan28 <sarujan28@hotmail.co.uk>
2023-11-07 18:15:14 +0000

    Script that picks random fruit from list of favourite fruits

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,7 @@
+import random
 # List of favourite fruits
 word_list = ['orange', 'apple', 'mango', 'melon', 'pear']
 print(word_list)
+# Picking random fruit from list of favourite fruits
+word = random.choice(word_list)
+print(word)
