31f5dcd23f28e344a1c75551116fd71d41dc2b61
Sarujan28 <sarujan28@hotmail.co.uk>
2023-11-07 19:52:46 +0000

    Adding check for if guess is in the word to script

--- milestone_3.py
+++ milestone_3.py
@@ -6,3 +6,11 @@ while True:
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
         continue
+# Check if guess is in the word
+import random
+word_list = ['orange', 'apple', 'mango', 'melon', 'pear']
+word = random.choice(word_list)
+if guess in word:
+    print(f'Good guess! {guess} is in the word.')
+else:
+    print(f'Sorry, {guess} is not in the word. Try again.')
