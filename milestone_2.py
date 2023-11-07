8b7b368a1a997927c0b0964a1c3592c05aa80601
Sarujan28 <sarujan28@hotmail.co.uk>
2023-11-07 18:17:15 +0000

    Script that asks you to guess a letter

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,9 @@ print(word_list)
 # Picking random fruit from list of favourite fruits
 word = random.choice(word_list)
 print(word)
+# Asking to guess a letter
+guess = input('Enter a single letter:')
+if len(guess) == 1 and guess.isalpha() == True:
+    print('Good Guess!')
+else:
+    print('Oops! That is not a vaild input.')
